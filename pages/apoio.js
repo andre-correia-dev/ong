24c5dd128979f1e89e80dcb41328c8be1542cf9b
kmaleao <udijonson@gmail.com>
2022-10-22 22:52:53 -0300

    apoio vereador

--- pages/apoio.js
+++ pages/apoio.js
@@ -1,10 +1,15 @@
+import styles from '../styles/apoio.module.css'
+
+
 export default function Apoio(){
     return(
         <>
-        <div>
-            <h1>Apoio</h1>
-        </div>
+        <div className={styles.apoio}>
+            <h1>Apoio:</h1>
+        <img src='/imagem/vereador.jpg'width='100%'></img>
+        <p>Vereador NEGO  trabalhando para nossa gente!</p>
         <button><a href='/'>voltar</a></button>
+        </div>
 
         </>
     )
