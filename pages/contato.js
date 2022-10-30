79ed4475401f7cc00a0fa3fba4606dc6d531cda8
kmaleao <udijonson@gmail.com>
2022-10-30 11:38:22 -0300

    contato

--- pages/contato.js
+++ pages/contato.js
@@ -12,10 +12,11 @@ export default function Contato(){
             
 
              <div className={styles.areacontato}>
-                <h1>redes sociais</h1>
+               <br></br>
+               <br></br>
+               <br></br>
                  <img src='/imagem/facebookicon.png'width={50}height={50}></img><a href='https://www.facebook.com/onarma/'>facebook</a><hr></hr>
-                 <img src='/imagem/instagranicon.jpg'width={50}height={50}></img><a href='/'>instagram</a><hr></hr>
-                 <img src='/imagem/twitericon.png'width={50}height={50}></img><a href='/'>...twiter</a><hr></hr>
+                
                  
                 
         </div>
